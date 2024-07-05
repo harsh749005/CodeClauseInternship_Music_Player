533fda0e8e75960f7af823bd27ec8faef6006a6c
Harsh <patelharsh749005@gmail.com>
2024-07-05 18:13:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,258 @@
+const data = [
+    {
+      id: 1,
+      songname: "After Hours",
+      songurl: "music/afterhour.mp3",
+      artist:"The Weekend",
+      imgurl: "https://lakecentralnews.com/wp-content/uploads/2020/02/Shah1-900x900.jpg",
+    },
+    {
+      id: 2,
+      songname: "Popular",
+      songurl: "music/Popular.mp3",
+      artist:"The Weekend",
+      imgurl: "https://i1.sndcdn.com/artworks-pyuGGE9VMNO4W3pO-ABa3PA-t500x500.jpg",
+    },
+    {
+      id: 3,
+      songname: "Starboy",
+      songurl: "music/starboy.mp3",
+      artist:"The Weekend",
+      imgurl: "https://source.boomplaymusic.com/group10/M00/EB/FC/527064fbcbed4ceaae6e2cee36112c06_464_464.jpg",
+    }
+    ,
+    {
+      id: 4,
+      songname: "Money",
+      songurl: "music/LISA - 'MONEY' EXCLUSIVE PERFORMANCE VIDEO.mp3",
+      artist:"Lisa",
+      imgurl: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Lisa_-_Money.jpg/220px-Lisa_-_Money.jpg",
+    },
+    {
+      id: 5,
+      songname: "Greedy",
+      songurl: "music/Tate McRae - Greedy (TikTok Version).mp3",
+      artist:"Tate McRae",
+      imgurl: "https://cdns-images.dzcdn.net/images/cover/b8ce27204ab9dbe887fdeb6985846c36/1900x1900-000000-80-0-0.jpg",
+    },
+    {
+      id: 6,
+      songname: "Under The Influence",
+      songurl: "music/Chris Brown - Under The Influence (Audio).mp3",
+      artist:"Chris Brown",
+      imgurl: "https://i1.sndcdn.com/artworks-wobOXi1OZrHTzGtM-eox4yA-t500x500.jpg",
+    },
+    {
+      id: 7,
+      songname: "Cheap thrills",
+      songurl: "music/cheap thrills sia ft sean paul (s l o w e d).mp3",
+      artist:"Sia",
+      imgurl: "https://upload.wikimedia.org/wikipedia/en/f/fc/Sia_Cheap_Thrills.png",
+    },
+    {
+      id: 8,
+      songname: "Stereo Love",
+      songurl: "music/stereo love [slowed to perfection].mp3",
+      artist:"Edward Maya and Vika Jigulina",
+      imgurl: "https://c.saavncdn.com/759/Stereo-Love-English-2010-20190924081753-500x500.jpg",
+    },{
+      id: 9,
+      songname: "Let Me Love You",
+      songurl: "music/DJ Snake ft. Justin Bieber - Let Me Love You [Lyric Video].mp3",
+      artist:"DJ Snake",
+      imgurl: "https://i.scdn.co/image/ab67616d0000b273212d776c31027c511f0ee3bc",
+    },{
+      id: 10,
+      songname: "Sorry",
+      songurl: "music/JUSTIN BIEBER - SORRY (LYRICS).mp3",
+      artist:"Justin Bieber",
+      imgurl: "https://upload.wikimedia.org/wikipedia/en/d/dc/Justin_Bieber_-_Sorry_%28Official_Single_Cover%29.png",
+    },{
+      id: 11,
+      songname: "Cold",
+      songurl: "music/Cold - Maroon 5  Ft. Future (Slowed TikTok Remix)(Lyrics) baby tell me how did you get so cold.mp3",
+      artist:"Maroon 5",
+      imgurl: "https://i1.sndcdn.com/artworks-000209709675-hwg59e-t500x500.jpg",
+    },{
+      id: 12,
+      songname: "People You Know",
+      songurl: "music/Selena Gomez - People You Know (Official Lyrics).mp3",
+      artist:"Selena Gomez",
+      imgurl: "https://i1.sndcdn.com/artworks-te6i00udhRZNv9zM-nKhutQ-t500x500.jpg",
+    },{
+      id: 13,
+      songname: "People You Know x Starboy (TikTok Mashup)",
+      songurl: "music/People You Know x Starboy (TikTok Mashup).mp3",
+      artist:"Selena Gomez & The Weekend",
+      imgurl: "https://c.saavncdn.com/003/People-You-Know-x-Starboy-Remix-English-2022-20221115013532-500x500.jpg",
+    },{
+      id: 14,
+      songname: "Blinding Lights",
+      songurl: "music/The Weeknd - Blinding Lights (Official Audio).mp3",
+      artist:"The Weeknd",
+      imgurl: "https://i1.sndcdn.com/artworks-T5KqOdidfrbpyddU-sGIJsA-t500x500.jpg",
+    },{
+      id: 15,
+      songname: "Levitating",
+      songurl: "music/Dua Lipa - Levitating Featuring DaBaby (Official Music Video).mp3",
+      artist:"Dua Lipa",
+      imgurl: "https://i1.sndcdn.com/artworks-IeJj19bzbXIrLiE3-Foa40Q-t500x500.jpg",
+    },{
+      id: 16,
+      songname: "One Kiss",
+      songurl: "music/Calvin Harris, Dua Lipa - One Kiss (Lyrics).mp3",
+      artist:"Dua Lipa",
+      imgurl: "https://i.scdn.co/image/ab67616d0000b2733ce13931214f0c268b588352",
+    },
+    {
+      id: 17,
+      songname: "Love Nwantiti",
+      songurl: "music/Ckay - Love Nwantiti (Tiktok Remix) Lyrics.mp3",
+      artist:"CKay",
+      imgurl: "https://hindisongs.com.in/siteuploads/thumb/sft4/1763_7.webp",
+    },{
+      id: 18,
+      songname: "Havana",
+      songurl: "music/Camila Cabello - Havana (Audio) ft. Young Thug.mp3",
+      artist:"Camila Cabello",
+      imgurl: "https://i.scdn.co/image/ab67616d0000b2736eb0b9e73adcf04e4ed3eca4",
+    },{
+      id: 19,
+      songname: "Shameless",
+      songurl: "music/Camila Cabello - Shameless (Lyrics).mp3",
+      artist:"Camila Cabello",
+      imgurl: "https://i1.sndcdn.com/artworks-sqGEJyozaJGeonM5-OObB4A-t500x500.jpg",
+    },{
+      id: 20,
+      songname: "MIDDLE OF THE NIGHT",
+      songurl: "music/Elley Duhé - Middle of the Night (Lyrics).mp3",
+      artist:"Elley Duhé",
+      imgurl: "https://i.scdn.co/image/ab67616d0000b27353a2e11c1bde700722fecd2e",
+    },
+  ];
+  
+  const playButton = document.querySelector(".play");
+  const audioTag = new Audio();
+  let currentTime = 0;
+  let isPlaying = false;
+  let currentSongIndex = 0;
+  
+  function formatDuration(duration) {
+    const minutes = Math.floor(duration / 60);
+    const seconds = Math.floor(duration % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  }
+  
+  function card() {
+    let clutter = "";
+    data.forEach((song) => {
+      clutter += `<div
+          key="${song.id}"
+          class="card relative min-w-44 h-44 bg-slate-400 rounded-md"
+          >
+          <img
+          src="${song.imgurl}"
+          alt="images"
+          class="cardimg absolute w-[100%] cursor-pointer rounded-md hover:opacity-80"
+          />
+          </div>`;
+    });
+    document.querySelector(".slider").innerHTML = clutter;
+  }
+  card();
+  
+  function setupSongCards() {
+    const cards = document.querySelectorAll(".cardimg");
+    cards.forEach((card, index) => {
+      card.addEventListener("click", () => {
+        playSong(index);
+      });
+    });
+  }
+  
+  function playSong(index) {
+    if (index !== currentSongIndex) {
+      audioTag.src = data[index].songurl;
+      currentSongIndex = index;
+      currentTime = 0;
+      audioTag.currentTime = currentTime;
+    }
+  
+    audioTag.play();
+    isPlaying = true;
+    playButton.className = "pause ri-pause-line text-2xl w-12 h-12 rounded-[50%] p-1 bg-white cursor-pointer";
+  
+    minCardSongInfo(data[index]);
+  
+    audioTag.addEventListener("timeupdate", () => {
+      currentTime = audioTag.currentTime;
+      updateSeekbar((currentTime / audioTag.duration) * 100);
+      document.querySelector(".current-time").textContent = formatDuration(currentTime);
+    });
+  
+    audioTag.addEventListener("ended", () => {
+      playNextSong();
+    });
+  
+    audioTag.addEventListener("loadedmetadata", () => {
+      document.querySelector(".duration").textContent = `/${formatDuration(audioTag.duration)}`;
+    }, { once: true });
+  }
+  
+  function stop() {
+    audioTag.pause();
+    currentTime = 0;
+    audioTag.currentTime = currentTime;
+    document.querySelector(".duration").textContent = `/${formatDuration(audioTag.duration)}`;
+    document.querySelector(".current-time").textContent = formatDuration(currentTime);
+    playButton.className = "play ri-play-fill text-2xl w-12 h-12 rounded-[50%] p-1 bg-white cursor-pointer";
+    isPlaying = false;
+  }
+  
+  function playNextSong() {
+    let nextIndex = (currentSongIndex + 1) % data.length;
+    playSong(nextIndex);
+  }
+  
+  function defaultSong() {
+    playButton.addEventListener("click", () => {
+      if (isPlaying) {
+        stop();
+      } else {
+        playSong(1)
+      }
+    
+    });
+  }
+  
+  function updateSeekbar(percentage) {
+    const seekbardot = document.querySelector(".dot");
+    seekbardot.style.left = percentage + "%";
+  }
+  
+  document.querySelector(".seekbar").addEventListener('click', (e) => {
+    const seekbar = e.currentTarget;
+    const seekbarWidth = seekbar.getBoundingClientRect().width;
+    const clickPosition = e.offsetX;
+    const clickPercentage = (clickPosition / seekbarWidth) * 100;
+    const newTime = (clickPercentage / 100) * audioTag.duration;
+    audioTag.currentTime = newTime;
+    currentTime = newTime;
+    updateSeekbar(clickPercentage);
+  });
+  
+  function minCardSongInfo(songData){
+    let imgUrl = songData.imgurl;
+    let songName = songData.songname;
+    let artistName = songData.artist;
+    const songMinImg = document.querySelector(".songMinImg");
+    const songNameSpan = document.querySelector(".songName");
+    const artistNameSpan = document.querySelector(".artistName");
+    songMinImg.src = imgUrl;
+    songNameSpan.textContent = songName;
+    artistNameSpan.textContent = artistName;
+  }
+  
+  defaultSong();
+  setupSongCards();
+  
